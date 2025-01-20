82b30ec6cde6e7efdd0fb197a66d01bdad8e4a97
u <u>
2025-01-20 03:57:17 +0300

    update

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 dnf install -y python3-build python3-py2pack
 python3 -m build -n -s . -o .
-py2pack generate --localfile *.tar.gz
-rpmbuild "-D_srcrpmdir ${outdir}" "-D_sourcedir ${outdir}" --bs "python-corp_gui.spec"
+var="$(cat variant.txt)"
+py2pack generate --localfile "python-copr_${var}"*.tar.gz
+rpmbuild "-D_srcrpmdir ${outdir}" "-D_sourcedir ${outdir}" --bs "python-corp_${var}.spec"
 
