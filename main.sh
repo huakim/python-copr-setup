fba71466779beb5617dbd3e7b4d71caf4382c5f4
u <u>
2025-01-20 04:08:32 +0300

    update

--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 dnf install -y python3-build python3-py2pack
 python3 -m build -n -s . -o .
 var="$(cat variant.txt)"
-py2pack generate --localfile "python-copr_${var}"*.tar.gz
-rpmbuild "-D_srcrpmdir ${outdir}" "-D_sourcedir ${outdir}" --bs "python-corp_${var}.spec"
+py2pack generate --localfile "copr_${var}"*.tar.gz
+rpmbuild "-D_srcrpmdir ${outdir}" "-D_sourcedir $PWD" --bs "python-copr_${var}.spec"
 
