b239e2fbf44db483c5b50b04725c47e2df2bff0b
u <u>
2025-01-20 04:31:25 +0300

    update

--- main.sh
+++ main.sh
@@ -1,4 +1,5 @@
-dnf install -y python3-build python3-py2pack
+curl -o $PWD/py.repo https://copr.fedorainfracloud.org/coprs/huakim/kde-plasma/repo/fedora-rawhide/huakim-kde-plasma-fedora-rawhide.repo
+dnf install --setopt=reposdir=$PWD -y python3-build python3-py2pack
 python3 -m build -n -s . -o .
 var="$(cat variant.txt)"
 py2pack generate --localfile "copr_${var}"*.tar.gz
